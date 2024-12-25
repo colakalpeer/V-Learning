825277373209df413e1261adfa01cd2d3e44a812
Alper Colak <colakalpeer93@gmail.com>
2024-12-25 09:31:16 +0300

    feat: trim was used to avoid possible spacing errors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function validateForm() {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+  const email = document.getElementById("email").valueitrim();
+  const password = document.getElementById("password").value.trim();
   const emailError = document.getElementById("email-error");
   const passwordError = document.getElementById("password-error");
 
